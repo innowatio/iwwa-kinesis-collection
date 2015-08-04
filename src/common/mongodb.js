2fb44756f6f1bcab4821515c3005ef4534124dad
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-08-05 00:46:22 +0200

    Fixed method name (collection, not getCollection)

--- src/common/mongodb.js
+++ src/common/mongodb.js
@@ -8,7 +8,7 @@ var connect = function (url) {
 export function upsert ({url, collectionName, query, element}) {
     return connect(url)
         .then(db => {
-            var collection = db.getCollection(collectionName);
+            var collection = db.collection(collectionName);
             return promisify(collection.upsert, collection)(query, element);
         });
 }
@@ -16,7 +16,7 @@ export function upsert ({url, collectionName, query, element}) {
 export function remove ({url, collectionName, query}) {
     return connect(url)
         .then(db => {
-            var collection = db.getCollection(collectionName);
+            var collection = db.collection(collectionName);
             return promisify(collection.remove, collection)(query);
         });
 }
