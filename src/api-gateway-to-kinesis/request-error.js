47994cd8d767b6078e4f44a389aa55a4cb04ee55
Emanuele Busi <emanuele.busi@mondora.com>
2015-11-19 17:58:56 +0100

    Unit tests for api gateway to kinesis

--- src/api-gateway-to-kinesis/request-error.js
+++ src/api-gateway-to-kinesis/request-error.js
@@ -1,6 +1,7 @@
 export default class RequestError extends Error {
 
     constructor (code, message) {
+        super();
         this.code = code;
         this.message = message;
     }
---
+++ test/api-gateway-to-kinesis.unit.js
@@ -0,0 +1,75 @@
+import BPromise from "bluebird";
+import chai, {expect} from "chai";
+import chaiAsPromised from "chai-as-promised";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+
+import RequestError from "../src/api-gateway-to-kinesis/request-error";
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+import apiGatewayToKinesis from "api-gateway-to-kinesis";
+
+describe("`apiGatewayToKinesis`", function () {
+
+    describe("`validate`", function () {
+
+        it("returns the request if the request body is valid", function () {
+            apiGatewayToKinesis.__Rewire__("checkSchema", sinon.stub().returns(true));
+            var validate = apiGatewayToKinesis.__get__("validate");
+            var instance = {
+                schema: {}
+            };
+            var request = {"body": {"element": 2}};
+            expect(validate.call(instance, request)).to.be.equals(request);
+        });
+
+        it("throws a `ValidationError` if the request body is not valid", function () {
+            apiGatewayToKinesis.__Rewire__("checkSchema", sinon.stub().returns(false));
+            var validate = apiGatewayToKinesis.__get__("validate");
+            var instance = {
+                schema: {}
+            };
+            var request = {"body": {"element": 2}};
+            const troublemaker = () => {
+                validate.call(instance, request);
+            };
+            expect(troublemaker).to.throw("ValidationError");
+            expect(troublemaker).to.throw(RequestError);
+        });
+    });
+
+    describe("`authorize`", function () {
+
+        it("if the user is authorized return the given request", function () {
+            const authorize = apiGatewayToKinesis.__get__("authorize");
+            const instance = {
+                authorizeApiRequest: sinon.stub().returns(
+                    BPromise.resolve(true))
+            };
+            const request = {};
+            const promise = authorize.call(instance, request);
+            return expect(promise).to.become(request);
+        });
+
+        it("if the user is not authorized throw an `Authentication Error`", function () {
+            const authorize = apiGatewayToKinesis.__get__("authorize");
+            const instance = {
+                authorizeApiRequest: sinon.stub().returns(
+                    BPromise.resolve(false)
+                )
+            };
+            const request = {};
+            const promise = authorize.call(instance, request);
+            return expect(promise).to.be.rejectedWith("AuthorizationError");
+        });
+    });
+
+    describe("`authenticate`", function () {
+
+        it("authenticate the user", function () {
+        });
+    });
+
+});
